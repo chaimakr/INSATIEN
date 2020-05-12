df211fb0885ca9fc73e3e306c58ccc5918ec8ef6
Chaimakr <chaima050@gmail.com>
2020-05-12 04:24:29 +0100

    Revert "Revert "Revert "beforeFOSuserbundle"""
    This reverts commit 992e32955c45c93d0e4401c705829c5569159b55.

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];

86c4fe2cd1045d4da85bf7bb74a939f588887f85
achourali <aliachour@insat.u-carthage.tn>
2020-06-08 20:53:02 +0100

    new home

--- src/Controller/PreSignInController.php
+++ src/Controller/PreSignInController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactMail;
 use App\Entity\User;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,6 +27,7 @@ class PreSignInController extends AbstractController
      */
     public function index()
     {
+
         if($this->getUser()){
             $role=$this->getUser()->getRoles()[0];
             if($role=='ROLE_STUDENT') return $this->redirect('/student');
@@ -52,9 +55,62 @@ class PreSignInController extends AbstractController
     /**
      * @Route("/anon", name="home")
      */
-    public function home()
+    public function home(Request $request, EntityManagerInterface $manager, \Swift_Mailer $mailer)
     {
-        return $this->render('pre_sign_in/home.html.twig');
+
+        $message = new ContactMail();
+        $mailform = $this->createFormBuilder($message)
+            ->add('fullName', TextType::class, [
+                "attr" => [
+                    "placeholder" => " add your full name",
+
+                ]
+            ])
+            ->add('email', TextType::class, [
+                "attr" => [
+                    "placeholder" => "example@insat.u-carthage.tn",
+                ]
+            ])
+            ->add('Subject', ChoiceType::class, [
+                "attr" => [
+                    "label" => "Choose One:"
+                ],
+                "choices" => [
+                    "service" => "General Customer Service",
+                    "Suggestions" => "Suggestions",
+                    "Feedbacks" => "Feedbacks"
+                ]
+
+            ])
+            ->add('Message', TextareaType::class, [
+                "attr" => [
+                    "placeholder" => "Message ",
+                ]
+            ])
+            ->add('send', SubmitType::class)
+            ->add('reset', ResetType::class)
+            ->getForm();
+        $mailform->handleRequest($request);
+
+        if ($mailform->isSubmitted() && $mailform->isValid()) {
+            $manager->persist($message);
+            $manager->flush();
+            $contact = $mailform->getData();
+            $msg = (new \Swift_Message('Nouveau contact'))
+                ->setFrom('insatien.help@gmail.com')
+                ->setTo('insatien.help@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'mailing/mail.html.twig', compact('contact')
+                    ),
+                    'text/html'
+                );
+            $mailer->send($msg);
+            return $this->redirect('feedback');
+        }
+        return $this->render('pre_sign_in/home.html.twig',[
+            "form"=>$mailform->createView()
+        ]);
     }
 
 
